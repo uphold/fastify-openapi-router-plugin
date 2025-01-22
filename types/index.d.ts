b8a9345846e05f216fbfaf52e9b5ae4fe63d7789
André Cruz <andre.cruz@uphold.com>
2025-01-22 19:17:48 +0000

    Fix typescript types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
 import { RouteOptions as FastifyRouteOptions, FastifyPluginCallback, FastifyRequest } from 'fastify'
 import OpenAPI from 'openapi-types';
 import { DECORATOR_NAME } from '../src/utils/constants'
-import { errors } from '../src/errors'
+import { errors } from '../src'
 
 declare module 'fastify' {
   interface FastifyRequest {
@@ -54,6 +54,14 @@ export interface PluginOptions {
 }
 
 export const openApiRouterPlugin: FastifyPluginCallback<PluginOptions>
+
+export function verifyScopes(providedScopes: string[], requiredScopes: string[]): string[]
+
+export function createSecurityHandlerError(error: Error, force?: boolean): typeof errors.SecurityHandlerError
+
+export function createScopesMismatchError(providedScopes: string[], requiredScopes: string[], missingScopes: string[]): typeof errors.ScopesMismatchError
+
+export function createUnauthorizedError(securityReport: SecurityReport): typeof errors.UnauthorizedError
 
 export { errors }
 
